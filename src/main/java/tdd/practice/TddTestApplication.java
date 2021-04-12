8fc33a18bed90614f5fc3cddd3e5e9467c6cbff7
yonmoyonmo <yonmosound@gmail.com>
2021-04-12 15:32:21 +0900

    tdd practice

---
+++ src/main/java/tdd/practice/TddTestApplication.java
@@ -0,0 +1,13 @@
+package tdd.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tdd/practice/TddTestApplicationTests.java
@@ -0,0 +1,13 @@
+package tdd.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
