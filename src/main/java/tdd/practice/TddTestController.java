5a1c114e5980db6724bc7198557d3c0e2494ef00
yonmoyonmo <yonmosound@gmail.com>
2021-04-13 17:01:35 +0900

    다시다시

--- src/main/java/tdd/practice/TddTestController.java
+++ src/main/java/tdd/practice/TddTestController.java
@@ -1,12 +1,12 @@
 package tdd.practice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import tdd.practice.entity.UserName;
 
 @RestController
 public class TddTestController {
-    @GetMapping("/")
-    public String hello(){
-        return "Hello";
-    }
+
 }
--- src/main/java/tdd/practice/entity/UserName.java
+++ src/main/java/tdd/practice/entity/UserName.java
@@ -1,23 +1,28 @@
 package tdd.practice.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
 public class UserName {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column
     private String userName;
 
     @CreationTimestamp
     private LocalDateTime createdTime;
 
+    public UserName(String name) {
+        this.userName = name;
+    }
 }
